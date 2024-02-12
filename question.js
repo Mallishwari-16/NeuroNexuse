291f299b3d62ecff5f50a3d7760507cb6b239a81
Mallishwari <138570460+Mallishwari-16@users.noreply.github.com>
2024-02-12 16:23:05 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,66 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What is an operating system?",
+      answer: "all of the mentioned",
+      options: [
+        "interface between the hardware and application programs",
+        "collection of programs that manages hardware resources",
+        "system service provider to the application programs",
+        "all of the mentioned"
+      ],
+    },
+    {
+      numb: 2,
+      question: "CPU scheduling is the basis of ___________",
+      answer: "multiprogramming operating systems",
+      options: [" multiprogramming operating systems",
+      " larger memory sized systems",
+      " multiprocessor systems",
+       "none of the mentioned"],
+    },
+    {
+      numb: 3,
+      question:
+        " To access the services of the operating system, the interface is provided by the ______",
+      answer: "System calls",
+      options: [
+        "Library",
+        " System calls",
+         "Assembly instructions",
+        "API",
+      ],
+    },
+    {
+      numb: 4,
+      question:
+        "Which one of the following errors will be handle by the operating system?",
+      answer: " all of the mentioned",
+      options: ["lack of paper in printet", "connection failure in the network",
+       "power failure", 
+       "all of the mentioned"],
+    },
+    {
+      numb: 5,
+      question: "Which command is used to remove a relation from an SQL ?",
+      answer: "Drop table",
+      options: ["Drop table", "Delete", "Purge", "Remove"],
+    },
+    {
+      numb: 6,
+      question: " What does OS X has?",
+      answer: "hybrid kernel",
+      options: ["Good", "microkernel",
+       "monolithic kernel",
+       "hybrid kernel"],
+    },
+    {
+      numb: 7,
+      question: "  Transient operating system code is a code that ____________",
+      answer: "comes and goes as needed",
+      options: ["comes and goes as needed", 
+      "is not easily accessible",
+       "never enters the memory space",
+       "hybrid kernel"],
+    },
+  ];
